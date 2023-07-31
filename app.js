478ff319b3b67d773a2798b3d85f723abb36b7b5
ClaraZalazar <zalazarmariaclara@gmail.com>
2023-07-31 19:17:05 -0300

    agregando animaciones

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const path = require('path');
+const path = require('path')
 
 const app = express();
 const PORT = 3030;
 
-const publicPath = path.resolve(__dirname, './public');
-app.use(express.static(publicPath));
+app.use(express.static('public'))
 
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')))
+app.get('/register',(req,res) => res.sendFile(path.join(__dirname,'views','register.html')))
+app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')))
 
-app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`))
+app.listen(PORT,() => console.log(`Servidor corriendo en http://localhost:${PORT}`));
